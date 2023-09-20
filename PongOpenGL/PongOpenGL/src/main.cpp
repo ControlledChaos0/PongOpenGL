a6e7b8341da30d1391d746a9d58a307ac083c87c
ControlledChaos0 <jade.spoon2003@gmail.com>
2023-09-20 13:44:24 -0400

    Starting on main and supporting methods

--- PongOpenGL/PongOpenGL/src/main.cpp
+++ PongOpenGL/PongOpenGL/src/main.cpp
@@ -15,26 +15,92 @@ const char* title = "Pong";
 */
 
 // initialize methods
-void initGLFW(unsigned int versionMajor, unsigned int versionMinor) {}
+void initGLFW(unsigned int versionMajor, unsigned int versionMinor) {
+	glfwInit();
+
+	//pass in window params
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, versionMajor);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, versionMinor);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+}
 
 //create window
-void createWindow(GLFWwindow*& window, const char* title, unsigned int width, unsigned int height, GLFWframebuffersizefun framebufferSizeCallback) {}
+void createWindow(GLFWwindow*& window, const char* title, unsigned int width, unsigned int height, GLFWframebuffersizefun framebufferSizeCallback) {
+	window = glfwCreateWindow(width, height, title, NULL, NULL);
+	if (!window) {
+		cleanup();
+		return;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+	glViewport(0, 0, width, height);
+}
 
 // callback window size change
-void framebufferSizeCallback(GLFWwindow* window, int width, int height) {}
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+	::width = width;
+	::height = height;
+}
 
 //load glad library
-bool loadGlad() {}
+bool loadGlad() {
+	return gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+}
 
 /*
 	shader methods
 */
 
 //read file
-std::string readFile(const char* filename) {}
+std::string readFile(const char* filename) {
+	std::ifstream file;
+	std::stringstream buf;
+
+	std::string ret = "";
+
+	//open file
+	file.open(filename);
+
+	if (file.is_open()) {
+		buf << file.rdbuf();
+		ret = buf.str();
+	}
+	else {
+		std::cout << "Could not open " << filename << std::endl;
+	}
+
+	file.close();
+
+	return ret;
+}
 
 //generate shader
-int genShader(const char* filepath, GLenum type) {}
+int genShader(const char* filepath, GLenum type) {
+	std::string shaderSrc = readFile(filepath);
+	const GLchar* shader = shaderSrc.c_str();
+
+	//build and compile the shader
+	int shaderObj = glCreateShader(type);
+	glShaderSource(shaderObj, 1, &shader, NULL);
+	glCompileShader(shaderObj);
+
+	//check for errors
+	int success;
+	char infoLog[512];
+	glGetShaderiv(shaderObj, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(shaderObj, 512, NULL, infoLog);
+		std::cout << "Error in shader compilation:" << std::endl << infoLog << std::endl;
+		return -1;
+	}
+
+	return shaderObj;
+}
 
 //generate shader program
 int genShaderProgram(const char* vertexShaderPath, const char* fragmentShaderPath) {}
@@ -74,7 +140,8 @@ template<typename T>
 void updateData(GLuint& bo, GLintptr offset, GLuint noElements, T* data) {}
 
 //set attribute pointers
-void setAttPointer(GLuint& bo, GLuint idx, GLint size, GLenum type, GLuint stride, GLuint divisor = 0) {}
+template<typename T>
+void setAttPointer(GLuint& bo, GLuint idx, GLint size, GLenum type, GLuint stride, GLuint offset, GLuint divisor = 0) {}
 
 //draw VAO
 void draw(VAO vao, GLenum mode, GLuint count, GLenum type, GLint indices, GLuint instanceCount = 1) {}
@@ -114,6 +181,99 @@ int main() {
 	//timing
 	double dt = 0.0;
 	double lastFrame = 0.0;
+
+	//initialization
+	initGLFW(3, 3);
+
+	//create window
+	GLFWwindow* window = nullptr;
+	createWindow(window, title, width, height, framebufferSizeCallback);
+	if (!window) {
+		std::cout << "Could not create window" << std::endl;
+		cleanup();
+		return -1;
+	}
+
+	//load glad
+	if (!loadGlad()) {
+		std::cout << "Could not init GLAD" << std::endl;
+		cleanup();
+		return -1;
+	}
+
+	//shaders
+	GLuint shaderProgram = genShaderProgram("main.vs", "main.fs");
+	setOrthographicProjection(shaderProgram, 0, width, 0, height, 0.0f, 1.0f);
+
+	//setup vertex data
+	float vertices[] = {
+		0.5f, 0.5f,
+		-0.5f, 0.5f,
+		-0.5f, -0.5f,
+		0.5f, -0.5f
+	};
+
+	//setup index data
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
+
+	float offsets[] = {
+		200.0f, 200.0f
+	};
+
+	float sizes[] = {
+		50.0f, 50.0f
+	};
+
+	//setup VAO/VBOs
+	VAO vao;
+	genVAO(&vao);
+
+	//pos VBO
+	genBufferObject<float>(vao.posVBO, GL_ARRAY_BUFFER, 2 * 4, vertices, GL_STATIC_DRAW);
+	setAttPointer<float>(vao.posVBO, 0, 2, GL_FLOAT, 2, 0);
+
+	// offset VBO
+	genBufferObject<float>(vao.offsetVBO, GL_ARRAY_BUFFER, 1 * 2, offsets, GL_DYNAMIC_DRAW);
+	setAttPointer<float>(vao.offsetVBO, 1, 2, GL_FLOAT, 2, 0, 1);
+
+	//size VBO
+	genBufferObject<float>(vao.sizeVBO, GL_ARRAY_BUFFER, 1 * 2, offsets, GL_DYNAMIC_DRAW);
+	setAttPointer<float>(vao.sizeVBO, 2, 2, GL_FLOAT, 2, 0, 1);
+
+	//EBO
+	genBufferObject<unsigned int>(vao.EBO, GL_ELEMENT_ARRAY_BUFFER, 3 * 2, indices, GL_STATIC_DRAW);
+
+	//unbind VBO and VAO
+	unbindBuffer(GL_ARRAY_BUFFER);
+	unbindVAO();
+
+	//render loop
+	while (!glfwWindowShouldClose(window)) {
+		//update time
+		dt = glfwGetTime() - lastFrame;
+		lastFrame += dt;
+
+		//input
+		processInput(window);
+
+		//clear screen for new frame
+		clearScreen();
+
+		//render object
+		bindShader(shaderProgram);
+		draw(vao, GL_TRIANGLES, 3 * 2, GL_UNSIGNED_INT, 0);
+
+		//swap frames
+		newFrame(window);
+	}
+
+	//cleanup memory
+	cleanup(vao);
+	deleteShader(shaderProgram);
+	cleanup();
 
 	return 0;
 }
